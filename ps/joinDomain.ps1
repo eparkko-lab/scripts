318b637696e2d861deef0d5495e6bdc30d251601
eparkko-lab <erik.parkkonen@yubico.com>
2020-09-02 11:02:02 -0700

    script for joining domain

--- ps/joinDomain.ps1
+++ ps/joinDomain.ps1
@@ -1,13 +1,15 @@
-param(
-    [String]
-    $DNSServerIPAddress,
+param(    
     [string]$Domain,
     [string]$Password
+    [string]$ComputerName
 
 
 )
-Write-Output "Joining the domain: $Domain"
 
+Write-Output "Renaming Computer to: $ComputerName"
+Rename-Computer -NewName $ComputerName
+
+Write-Output "Joining the domain: $Domain"
 $user = "$Domain\admin"
 $pass = ConvertTo-SecureString "$Password" -AsPlainText -Force
 $DomainCred = New-Object System.Management.Automation.PSCredential $user, $pass
